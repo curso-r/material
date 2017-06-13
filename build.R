9637cd2d0d65239ed48812107c9951a1f288d886
williamorim <thewilliam89@gmail.com>
2017-06-13 16:16:07 -0300

    Hugo version

--- build.R
+++ build.R
@@ -35,5 +35,5 @@ download_and_extract_pu <- function(power_up){
 purrr::walk(power_ups, download_and_extract_pu)
 
 # install hugo & build site
-blogdown::install_hugo(version = "0.17")
+blogdown::install_hugo(version = "0.21")
 blogdown::build_site()
